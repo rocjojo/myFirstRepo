a0af7f4af8b0bb788f3041ecc6e1b1f501441bff
Joel Rochat <rocjojo@gmail.com>
2012-06-12 20:20:08 +0200

    initial project version

---
+++ FirstApp.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.io.FileOutputStream;
+
+class FirstApp {
+    public static void main (String[] args) throws Exception {
+        String root = "/tmp/poa/in" ;
+        StringBuilder path = new StringBuilder(root) ;
+        int id = 1000 ;
+        String fileName = String.valueOf(id) ;
+        for (int i = 100000 ; i >= 1 ; i/=10) {
+            path.append("/").append(id/i);
+            id = id % i ;
+        }
+        new File(path.toString()).mkdirs();
+
+    }
+}
+
